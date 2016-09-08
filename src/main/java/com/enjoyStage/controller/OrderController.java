85df1dd8f88c8b734d695cc8647988a665decfc4
sharysy <1287641592@qq.com>
2016-09-09 01:57:43 +0800

    shiyue - add OrderController

---
+++ src/main/java/com/enjoyStage/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.enjoyStage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/order")
+public class OrderController {
+	@RequestMapping(value="/payLoad")
+	public ModelAndView payLoad(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("alipay");
+		return mv;
+	}
+}
