0ceaa8068a0b7d13a58c35ee54ba62e920e2bd84
Demo-tong <18829292377@163.com>
2016-09-08 19:47:12 +0800

    tongyang - modify code style

--- src/main/java/com/enjoyStage/service/impl/UserServiceImpl.java
+++ src/main/java/com/enjoyStage/service/impl/UserServiceImpl.java
@@ -14,30 +14,30 @@ public class UserServiceImpl implements IUserService {
 
 	@Autowired
 	private IUserDAO userDao;
-	public boolean selectByUsername(String username, String password){
-		boolean bool=false;
-		User user=userDao.selectByUsername(username, password);
-		if(user!=null){
-			bool=true;
+
+	public boolean selectByUsername(String username, String password) {
+		boolean bool = false;
+		User user = userDao.selectByUsername(username, password);
+		if (user != null) {
+			bool = true;
 		}
 		return bool;
 	}
-	
-	public boolean insertUser(String username, String password, String password_t){
-		//先判断用户两次输入密码是否一致
-		if(password.equals(password_t) == false){
-			// 密码不一致返回false
-			return false;
-		}else{
-			/*
-			 * 通过调用insertUser方法，在其中判断注册名是否合法
-			 * 若合法，则插入数据库中，返回true；否则，注册失败返回false
-			 */
+
+	public boolean insertUser(String username, String password, String password_t) {
+
+		/*
+		 * 先判断用户两次输入密码是否一致 通过调用insertUser方法，在其中判断注册名是否合法
+		 * 若合法，则插入数据库中，返回true；否则，注册失败返回false
+		 */
+		if (password.equals(password_t)) {
 			return userDao.insertUser(username, password);
+
 		}
-		
+		// 密码不一致返回false
+		return false;
 	}
-	
+
 	public List<User> findAll() {
 		// TODO Auto-generated method stub
 		return userDao.findAll();
